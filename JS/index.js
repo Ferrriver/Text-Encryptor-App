d0204ea9fc61fccee2195cec1757261f176d6aaa
Fernanda <river.fer.ric@gmail.com>
2023-05-10 14:57:08 -0600

    Variables name updated

--- JS/index.js
+++ JS/index.js
@@ -1,54 +1,54 @@
 document.querySelector("#card-result").style.display = "none";
 
-var texto;
-var resultado;
-var tarjetaResultado;
-var tarjetaVacia;
+var text;
+var result;
+var cardResult;
+var emptyCard;
 var encryptButton = document.querySelector("#encrypt-btn");
 var decryptButton = document.querySelector("#decrypt-btn");
 
 
-function  encriptar() {
+function  encrypt() {
    
-texto = document.querySelector("#textArea").value;
-resultado = document.querySelector("#resultado");
-tarjetaResultado = document.querySelector("#card-result");
-tarjetaVacia = document.querySelector("#card-no-result");
+    text = document.querySelector("#textArea").value;
+    result = document.querySelector("#result");
+    cardResult = document.querySelector("#card-result");
+    emptyCard = document.querySelector("#card-no-result");
 
-    let textoCifrado = texto
+    let textEncrypted = text
         .replace(/e/gi, "enter")
         .replace(/i/gi, "imes")
         .replace(/a/gi, "ai")
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
                             
-    if (texto.length != 0) {
-        resultado.value = textoCifrado;
-        tarjetaVacia.style.display = "none";
-        tarjetaResultado.style.display = "flex"; 
+    if (text.length != 0) {
+        result.value = textEncrypted;
+        emptyCard.style.display = "none";
+        cardResult.style.display = "flex"; 
         $(encryptButton).popover('hide');
     } else {
         $(encryptButton).popover('show');
     }
 }
 
-function desencriptar() {
-    texto = document.querySelector("#textArea").value;
-    resultado = document.querySelector("#resultado");
-    tarjetaResultado = document.querySelector("#card-result");
-    tarjetaVacia = document.querySelector("#card-no-result");
+function decrypt() {
+    text = document.querySelector("#textArea").value;
+    result = document.querySelector("#result");
+    cardResult = document.querySelector("#card-result");
+    emptyCard = document.querySelector("#card-no-result");
 
-    let textoCifrado = texto
+    let textEncrypted = text
         .replace(/enter/gi, "e")
         .replace(/imes/gi, "i")
         .replace(/ai/gi, "a")
         .replace(/ober/gi, "o")
         .replace(/ufat/gi, "u");
 
-    if (texto.length != 0) {
-        resultado.value = textoCifrado;
-        tarjetaVacia.style.display = "none";
-        tarjetaResultado.style.display = "flex";
+    if (text.length != 0) {
+        result.value = textEncrypted;
+        emptyCard.style.display = "none";
+        cardResult.style.display = "flex";
         $(decryptButton).popover('hide');
     } else {
         $(decryptButton).popover('show');
@@ -56,6 +56,6 @@ function desencriptar() {
 }
 
 function copiarTexto() {
-    resultado.select();
+    result.select();
     document.execCommand("copy");
 }
